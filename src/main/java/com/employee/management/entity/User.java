382b3e9cb114600524e1f15f34170ee0c15d1b5a
Elshen105 <elshennaghizade1@gmail.com>
2023-12-13 23:49:22 +0400

    remove import

--- src/main/java/com/employee/management/entity/User.java
+++ src/main/java/com/employee/management/entity/User.java
@@ -1,7 +1,6 @@
 package com.employee.management.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
