bc04fc2eb9feeafa0e3098f4f7a79b47450c4c7b
Elshen105 <elshennaghizade1@gmail.com>
2023-12-13 19:41:20 +0400

    Add Validation for UserRequest class and using @Email, @Size and Pattern

--- src/main/java/com/employee/management/model/UserRequest.java
+++ src/main/java/com/employee/management/model/UserRequest.java
@@ -1,5 +1,9 @@
 package com.employee.management.model;
 
+import com.employee.management.util.ValidationConstants;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +19,16 @@ import java.util.Set;
 public class UserRequest {
     private String name;
     private String surname;
+    @Email(message = "Make sure the email you enter has an sign: @")
     private String email;
     private boolean status;
     private String username;
+    @Size(min = ValidationConstants.PASSWORD_MIN_LENGTH,
+            max = ValidationConstants.PASSWORD_MAX_LENGTH,
+            message = "Password must be at least 8 characters long"
+    )
+    @Pattern(regexp = ValidationConstants.PASSWORD_REGEX,
+            message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character")
     private String password;
 
     private Set<RoleDto> role;
