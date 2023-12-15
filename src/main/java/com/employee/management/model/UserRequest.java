2d52ba77600fcb040104f1d74fff7671bb9ad70a
Elshen105 <elshennaghizade1@gmail.com>
2023-12-15 15:20:55 +0400

    The content of the message has been changed

--- src/main/java/com/employee/management/model/UserRequest.java
+++ src/main/java/com/employee/management/model/UserRequest.java
@@ -25,7 +25,7 @@ public class UserRequest {
     private String username;
     @Size(min = ValidationConstants.PASSWORD_MIN_LENGTH,
             max = ValidationConstants.PASSWORD_MAX_LENGTH,
-            message = "Password must be at least 8 characters long"
+            message = "The password must be at least 8 and at most 20 characters long"
     )
     @Pattern(regexp = ValidationConstants.PASSWORD_REGEX,
             message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character")
